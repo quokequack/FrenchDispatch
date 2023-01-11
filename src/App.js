63cc1ddb8cb9ba7a648c9daf70524a6bddfa4a91
quokequack <roymustangtaizo@gmail.com>
2023-01-11 17:37:36 -0300

    stylized signin page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import '../src/index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" element={<HomeScreen />}/>
           <Route path="/about" element={<About />}/>
           <Route path="/signup" element={<SignUp />}/>
+          <Route path="/signin" element={<SignIn />}/>
         </Routes>
       </BrowserRouter>
     </>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,32 @@
 import React from "react";
+import '../styles/signup.css';
+import { Link } from "react-router-dom";
 
 export default function SignUp(){
     return(
-        <p>This is signup page, not finished yet :(</p>
+        <body className="signup">
+            <div className="headerSignUp">
+                <div className="line"></div>
+                <div className="text">
+                    <h1>JOIN US!</h1>
+                </div>
+            </div>
+            <div className="paragraph">
+            <p>The magazine is dying but you can have access to the amazing and exciting last edition!</p>
+            </div>
+            <div className="form">
+                <p className='formTxt'>NAME</p>
+                <input type="text" className="formBox"></input>
+                <p className="formTxt">EMAIL</p>
+                <input type="email" className="formBox"></input>
+                <p className="formTxt">PASSWORD</p>
+                <input type="password" className="formBox"></input>
+                <button type="submit" className="submitBtn">Sign Up</button>
+            </div>
+            <div className="footerSignUp">
+                <p className="paragraph2">Do you already have an account?</p>
+                <Link to="/signin" className="signinBtn"><p className="suBtnTxt">Sign In</p></Link>
+            </div>
+        </body>
     );
 };
